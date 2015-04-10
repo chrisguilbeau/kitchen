8a4e8ad8c02668da3df12e8322fb02a39316d101
cg <cg@cg-air.local>
2015-04-10 13:53:59 -0500

    removed pyc

--- configuration.py
+++
@@ -1,9 +0,0 @@
-GOOGLE_API_KEY = ''
-PORT = 8080
-DEBUG = False
-HOME = '917+Truman+Lane,+Cedar+Park,+TX+78613-3847,+USA'
-WORK = '100+Congress+Avenue,+Austin,+TX+78701,+USA'
-WEATHER_LOC = 'Cedar Park'
-WEATHER_LAT = 30.5169
-WEATHER_LON = -97.8101
-CAL_IFRAME = "https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=chris.guilbeau%40gmail.com&amp;color=%23BE6D00&amp;src=elizabeth.guilbeau%40gmail.com&amp;color=%23705770&amp;src=sfhoql5tai8rukpsva8iu3l5v4%40group.calendar.google.com&amp;color=%235A6986&amp;src=ahg2r7oqf8hg8incr95vr0j82c%40group.calendar.google.com&amp;color=%2328754E&amp;src=cguilbeau%40apps.zogotech.com&amp;color=%23A32929&amp;src=en.usa%23holiday%40group.v.calendar.google.com&amp;color=%23125A12&amp;ctz=America%2FChicago"
